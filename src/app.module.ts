e4c15ee278fac1e81a0fdad2c390f761c20e510c
Тимур Латуш <timurlatus@Timurs-MacBook-Pro-2.local>
2025-11-30 12:16:59 +0100

    connected to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,27 @@ import { PortfolioCompaniesModule } from './portfolio-companies/portfolio-compan
 import { NewsModule } from './news/news.module';
 import { ForecastsModule } from './forecasts/forecasts.module';
 import { CompanyModule } from './company/company.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { Forecast } from './forecasts/entities/forecast.entity';
+import { NewsItem } from './news/entities/news.entity';
+import { PortfolioCompany } from './portfolio-companies/entities/portfolio-company.entity';
+import { Company } from './company/entities/company.entity';
+import { Transaction } from './transactions/entities/transaction.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [UserModule, PortfolioCompaniesModule, NewsModule, ForecastsModule, CompanyModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    url: process.env.SUPABASE_DB_URL,
+    entities: [User, PortfolioCompany, NewsItem, Forecast, Company, Transaction],
+    synchronize: true, 
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+    UserModule, PortfolioCompaniesModule, NewsModule, ForecastsModule, CompanyModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly dataSource: DataSource) {}
+
+  async create(createUserDto: CreateUserDto) {
+
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+
+
   }
 
   findOne(id: number) {
